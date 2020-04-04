89ab05c98110bf33b5d72f9ea6f226ee5c37ce6e
Forest <forestkzanderson@gmail.com>
2020-04-03 20:44:58 -0400

    Clustering all drones by proximity

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -1,7 +1,9 @@
 extends Node
 
-const NUM_OF_DRONES = 500
+const NUM_OF_DRONES = 1000
 const NUM_OF_CLUSTERS = int((sqrt(8 * NUM_OF_DRONES + 1) - 1) / 2)
+const DRONES_PER_CLUSTER = int(NUM_OF_DRONES / NUM_OF_CLUSTERS)
+const REMAINDER_DRONES = NUM_OF_DRONES % NUM_OF_CLUSTERS
 
 # In bytes/second
 const RECEPTION_RATE = 10000
@@ -11,3 +13,4 @@ const TRANSMISSION_RATE = 10000
 
 var drone_cluster_lookup = {}
 var drones = []
+var area = [100, 100, 100]
--- Scripts/World.gd
+++ Scripts/World.gd
@@ -11,31 +11,79 @@ func _ready():
 	for _i in range(Globals.NUM_OF_DRONES):
 		var new_drone = drone_scene.instance()
 		new_drone.translate(Vector3(
-			randi() % 300 - 150,
-			randi() % 50 - 25,
-			randi() % 300 - 150
+			randi() % Globals.area[0] - Globals.area[0] / 2,
+			randi() % Globals.area[1] - Globals.area[1] / 2,
+			randi() % Globals.area[2] - Globals.area[2] / 2
 		))
 		
 		Globals.drones.append(new_drone)
 		self.add_child(new_drone)
 
-	group_drones()
+	kmeans()
 
-# Group drones by just dealing cards
-func group_drones():
-	# Create arrays for the clusters in a dict
+func kmeans():
 	for i in range(Globals.NUM_OF_CLUSTERS):
 		clusters[i] = []
+	
+	var centroids = []
+	var drones_available = Globals.drones.duplicate()
+	
+	for cluster_num in range(Globals.NUM_OF_CLUSTERS):
+		# Get cluster size, account for remainder drones
+		var cluster_size = Globals.DRONES_PER_CLUSTER
+		if cluster_num < Globals.REMAINDER_DRONES:
+			cluster_size += 1
 
-	# Deal each drone to a cluster
-	for i in range(Globals.drones.size()):
-		var drone = Globals.drones[i]
-		var cluster = i % Globals.NUM_OF_CLUSTERS
+		# Pick the centroids
+		var this_centroid = Vector3(
+			randi() % Globals.area[0] - Globals.area[0] / 2,
+			randi() % Globals.area[1] - Globals.area[1] / 2,
+			randi() % Globals.area[2] - Globals.area[2] / 2
+		)
+		centroids.append(this_centroid)
+		
+		var closest_drones = []
+		
+		for drone_num in drones_available.size():
+			var distance = this_centroid.distance_squared_to(Vector3(
+				drones_available[drone_num].translation.x,
+				drones_available[drone_num].translation.y,
+				drones_available[drone_num].translation.z
+			))
 
-		clusters[cluster].append(drone)
+			if closest_drones.size() == 0:
+				# Add the drone automatically if it's the only one so far
+				closest_drones.insert(0, [distance, drones_available[drone_num], drone_num])
+			else:
+				# Search through the list to see if it can fit
+				var found_place = false
+				for i in range(closest_drones.size()):
+					if distance < closest_drones[i][0]:
+						closest_drones.insert(i, [distance, drones_available[drone_num], drone_num])
+						found_place = true
+						break
 
-		# Add drone objects to a lookup table
-		Globals.drone_cluster_lookup[drone] = cluster
+				# Add to the end if it didn't fit anywhere else. The loop abover doesn't do this
+				if !found_place:
+					closest_drones.append([distance, drones_available[drone_num], drone_num])
+
+				# If we're over size, remove the last element
+				if closest_drones.size() > cluster_size:
+					closest_drones.pop_back()
+			
+		# Make a list of all the drones to remove
+		var remove_list = []
+		for i in range(closest_drones.size()):
+			clusters[cluster_num].append(closest_drones[i][1])
+			remove_list.append(closest_drones[i][2])
+
+		# Sort and invert the indicies so removal happens from
+		# largest first, and doesn't shift the array while we're removing
+		remove_list.sort()
+		remove_list.invert()
+
+		for i in range(remove_list.size()):
+			drones_available.remove(remove_list[i])
 
 	# Color them
 	for cluster in clusters.values():
