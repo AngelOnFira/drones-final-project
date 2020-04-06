3276a15584ab0f7a73866048e4e593024419465b
Forest <forestkzanderson@gmail.com>
2020-04-05 20:41:19 -0400

    Messages sending all the way

--- Scripts/Drone.gd
+++ Scripts/Drone.gd
@@ -11,38 +11,37 @@ var download_rate = 10000
 # In bytes/second
 var upload_rate = 10000
 
-var time_until_next_message = 0
+var time_until_next_message = 0.1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	roll_time_next_message()
+    pass
 
 func roll_time_next_message():
-	#self.time_until_next_message = rand_range(1,11)
-	self.time_until_next_message = 0.1
+    self.time_until_next_message = rand_range(1,11)
 
 func send_message():
-	var drones = Globals.drone_cluster_lookup.keys()
+    var drones = Globals.drone_cluster_lookup.keys()
 
-	var destination = self
-	while destination == self:
-		destination = drones[int(rand_range(0, drones.size()))]
+    var destination = self
+    while destination == self:
+        destination = drones[int(rand_range(0, drones.size()))]
 
-	var new_message = message.new()
+    var new_message = message.new()
 
-	new_message.final_destination = destination
-	new_message.current_uploader = self
-	new_message.message_size = 15000
+    new_message.final_destination = destination
+    new_message.current_uploader = self
+    new_message.message_size = 1500000
 
-	new_message.start()
-
-	outgoing_queue.append(new_message)
-	get_node('/root/Main/Messages').add_child(new_message)
+    get_node('/root/Main/Messages').add_child(new_message)
+    
+    new_message.start()
 
 func _process(delta):
-	time_until_next_message -= delta
-	if time_until_next_message <= 0.0:
-		send_message()
+    time_until_next_message -= delta
+    if time_until_next_message <= 0.0:
+        roll_time_next_message()
+        send_message()
 
-	if outgoing_queue.size() > 0:
-		outgoing_queue[0].upload(delta)
+    if outgoing_queue.size() > 0:
+        outgoing_queue[0].upload(delta)
--- Scripts/Message.gd
+++ Scripts/Message.gd
@@ -4,26 +4,60 @@ var message_size
 var remaining_transfer
 var final_destination
 
+var destination_channel
+var starting_channel
+var destination_channel_head
+
 var current_downloader
 var current_uploader
 
+var visual_link = null
+
 func start():
-    var my_channel = Globals.drone_cluster_lookup[current_uploader]
-    var destination_channel = Globals.drone_cluster_lookup[final_destination]
-    var my_channel_head = Globals.channel_head_lookup[my_channel]
+    starting_channel = Globals.drone_cluster_lookup[current_uploader]
+    destination_channel = Globals.drone_cluster_lookup[final_destination]
+    destination_channel_head = Globals.channel_head_lookup[destination_channel]
+    var my_channel_head = Globals.channel_head_lookup[starting_channel]
     
-    if my_channel == destination_channel:
+    if starting_channel == destination_channel:
         # If the message is in the same channel
         current_downloader = final_destination
     else:
         current_downloader = my_channel_head
 
+    current_uploader.outgoing_queue.push_back(self)
+    current_downloader.incoming_queue.append(self)
+
     remaining_transfer = message_size
+    
+    update_visual_link()
+
+func update_visual_link():
+    if visual_link:
+        get_child(0).queue_free()
+        
+    visual_link = CSGCylinder.new()
+    self.add_child(visual_link)
+    
+    visual_link.global_translate((current_downloader.translation + current_uploader.translation) / 2)
+    
+    visual_link.look_at(
+        current_downloader.translation,
+        Vector3(0, 0, 0)
+    )
+    
+    visual_link.rotate_x(deg2rad(90))
+    visual_link.set_scale(Vector3(1, 100, 1))
+    # look at one
+    # scale to half the distance
 
 func upload(delta):
     var download_speed = current_downloader.download_rate
-    var num_jobs = current_downloader.incoming_queue.size()
-    var upload_speed = min(download_speed / num_jobs, current_uploader.upload_rate)
+    var num_jobs = max(current_downloader.incoming_queue.size(), 1)
+    var upload_speed = min(
+        download_speed / num_jobs,
+        current_uploader.upload_rate * delta
+    )
 
     remaining_transfer -= upload_speed
 
@@ -31,4 +65,28 @@ func upload(delta):
         next_node()
 
 func next_node():
-    pass
+    var current_channel = Globals.drone_cluster_lookup[current_downloader]
+    
+    current_uploader.outgoing_queue.pop_front()
+
+    var download_queue = current_downloader.incoming_queue
+    download_queue.remove(download_queue.find(self))
+    
+    # if we're at destination
+    if current_downloader == final_destination:
+        self.queue_free()
+        print("message recieved")
+        return
+
+    current_uploader = current_downloader
+    remaining_transfer = message_size
+
+    if current_channel == starting_channel:
+        # if we're at our cluster head
+        current_downloader = destination_channel_head
+    else:
+        # if we're at their cluster head
+        current_downloader = final_destination
+
+    current_downloader.incoming_queue.append(self)
+    update_visual_link()
